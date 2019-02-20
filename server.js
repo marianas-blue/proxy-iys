5c8dd7952d0b8f35862d7124ed5f8e17503b55f0
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2019-02-19 20:29:04 -0800

    Setup Proxy Server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors())
+
+// Static Bundles
+app.use('/comp/add_to_cart-chris', express.static(`${__dirname}/comp/add_to_cart-chris/client/dist`));
+app.use('/comp/product-carousel-pablo', express.static(`${__dirname}/comp/product-carousel-pablo/client/dist`));
+app.use('/comp/product-reviews-victor', express.static(`${__dirname}/comp/product-reviews-victor/client/dist`));
+app.use('/comp/product-gallery-summary', express.static(`${__dirname}/comp/product-gallery-summary/dist`));
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`)
+});
+
+app.listen(8000);
