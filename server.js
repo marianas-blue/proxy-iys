36984890dd528083f1ee48b3eeb80bd4875f627e
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2019-02-22 14:24:00 -0800

    Setup Docker & EC2

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 
 const app = express();
 
 app.use(cors())
 
-// Static Bundles
-app.use('/comp/add_to_cart-chris', express.static(`${__dirname}/comp/add_to_cart-chris/client/dist`));
-app.use('/comp/product-carousel-pablo', express.static(`${__dirname}/comp/product-carousel-pablo/client/dist`));
-app.use('/comp/product-reviews-victor', express.static(`${__dirname}/comp/product-reviews-victor/client/dist`));
-app.use('/comp/product-gallery-summary', express.static(`${__dirname}/comp/product-gallery-summary/dist`));
+app.use(compression());
+
+// // Static Bundles
+// app.use('/comp/add_to_cart-chris', express.static(`${__dirname}/comp/add_to_cart-chris/client/dist`));
+// app.use('/comp/product-carousel-pablo', express.static(`${__dirname}/comp/product-carousel-pablo/client/dist`));
+// app.use('/comp/product-reviews-victor', express.static(`${__dirname}/comp/product-reviews-victor/client/dist`));
+// app.use('/comp/product-gallery-summary', express.static(`${__dirname}/comp/product-gallery-summary/dist`));
 
 app.use(express.static(`${__dirname}/public`));
 
@@ -17,4 +20,4 @@ app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`)
 });
 
-app.listen(8000);
+app.listen(80);
