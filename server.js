3eee365402589038b3faac96deb8d066a7067b22
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2019-02-23 12:05:11 -0800

    Create redirect for non viable client routes

--- server.js
+++ server.js
@@ -4,20 +4,18 @@ const compression = require('compression');
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(compression());
 
-// // Static Bundles
-// app.use('/comp/add_to_cart-chris', express.static(`${__dirname}/comp/add_to_cart-chris/client/dist`));
-// app.use('/comp/product-carousel-pablo', express.static(`${__dirname}/comp/product-carousel-pablo/client/dist`));
-// app.use('/comp/product-reviews-victor', express.static(`${__dirname}/comp/product-reviews-victor/client/dist`));
-// app.use('/comp/product-gallery-summary', express.static(`${__dirname}/comp/product-gallery-summary/dist`));
+app.use('/files', express.static(`${__dirname}/public`));
 
-app.use(express.static(`${__dirname}/public`));
+app.get('/products/:id', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
 
 app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`)
-});
+  res.redirect('/products/1');
+})
 
 app.listen(80);
