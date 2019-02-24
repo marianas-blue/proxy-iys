f0ac70bc30ec27013e44733de694c2897775eca7
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2019-02-23 16:16:44 -0800

    Add Favicon support

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
+const path = require('path');
+const favicon = require('serve-favicon')
 
 const app = express();
 
 app.use(cors());
 
 app.use(compression());
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use('/files', express.static(`${__dirname}/public`));
 
